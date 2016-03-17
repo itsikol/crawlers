24d6e89ac4e8537f8a86e2f32ec8943d4edc99b8
lokendras395 <lokendras395@gmail.com>
2016-03-18 03:12:08 +0530

    Create catamazon.py

---
+++ catamazon.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+import requests
+f=open("catlink.txt","w+")
+r=requests.get("http://www.amazon.in/gp/site-directory/ref=nav_shopall_fullstore")
+data=r.text
+
+soup=BeautifulSoup(data)
+tbl = soup.find('table',{'id':'shopAllLinks'})
+lis = tbl.find_all("li")
+for tdd in lis:
+  n = tdd.find('a').get('href')
+  print n
+  f.write('www.amazon.in'+n+'\n')
